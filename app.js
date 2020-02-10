70579275708250590339f090ebeb80da02b01fb6
Mario <m.nikolov@next-it.bg>
2020-02-10 14:03:10 +0200

    add images under the previewd one; focus previewed image in the list;

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const carouselSlide = document.querySelector('.carousel-slide');
 const carouselImages = document.querySelectorAll('.carousel-slide img');
+const imagesLineup = document.querySelector('.images-lineup');
+let smallImages = [];
 
 //Buttons
 const prevBtn = document.querySelector('#prevBtn');
@@ -11,7 +13,12 @@ const size = carouselImages[0].clientWidth;
 
 carouselSlide.style.transform = `translateX(${-size * counter}px)`;
 
-// Button listeners
+// Event listeners
+document.addEventListener('DOMContentLoaded', () => {
+  showSmallImages();
+  focusPreviewedImg();
+});
+
 nextBtn.addEventListener('click', () => {
   if (counter >= carouselImages.length - 1) return;
   carouselSlide.style.transition = 'transform 0.5s ease-in-out';
@@ -38,4 +45,34 @@ carouselSlide.addEventListener('transitionend', () => {
     counter = carouselImages.length - counter;
     carouselSlide.style.transform = `translateX(${-size * counter}px)`;
   }
+
+  focusPreviewedImg();
 });
+
+// Functions
+function showSmallImages() {
+  carouselImages.forEach(img => {
+    const imgId = img.getAttribute('id');
+
+    if (imgId !== 'firstClone' && imgId !== 'lastClone') {
+      const smallImage = document.createElement('img');
+      smallImage.setAttribute('src', img.getAttribute('src'));
+      imagesLineup.appendChild(smallImage);
+    }
+  });
+
+  smallImages = document.querySelectorAll('.images-lineup img');
+}
+
+function focusPreviewedImg() {
+  smallImages.forEach(img => {
+    img.style.border = '';
+    img.classList.remove('previewed');
+  });
+
+  smallImages[counter - 1].classList.add('previewed');
+}
+
+function selectImage(img) {
+  counter = counter + 1;
+}
